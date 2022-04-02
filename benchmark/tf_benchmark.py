05c4fcfce3526272664d10f91c728351833f2afa
handelae <handelae@oregonstate.edu>
2022-04-01 18:50:40 -0700

    getting there

--- benchmark/tf_benchmark.py
+++ benchmark/tf_benchmark.py
@@ -4,57 +4,17 @@
 # Description: Benchmarks the performance of the Tensorflow models
 #
 # Import packages
-from os.path import isfile, join
-
 from tf_evaluation import evaluate_tflite
 
-model_path = '../models/coco_ssd_mobilenet_v1_1.0_quant_2018_06_29'
-dataset_path = 'Prediction_set/'
-# evaluate_tflite(model_path + '/detect.tflite', "/home/erik/PycharmProjects/ml/test_img") .. kinda works, basiclly just calls imag_classifier migh tneed to use in area for testing?
-
-import numpy as np
 import os
-# !pip install -q tflite-model-maker
-# !pip install -q tflite-support
-from tflite_model_maker.config import ExportFormat, QuantizationConfig
-from tflite_model_maker import model_spec
-from tflite_model_maker import object_detector
-
-from tflite_support import metadata
-
-import tensorflow as tf
-
-assert tf.__version__.startswith('2')
-
-tf.get_logger().setLevel('ERROR')
-from absl import logging
-
-logging.set_verbosity(logging.ERROR)
-
-
 def benchmark():
     BASE_DIR = os.path.join(os.path.dirname(__file__), '..')
 
     models = [f for f in os.listdir(BASE_DIR + "/models") if not f.startswith('.')]
 
-    test_data = object_detector.DataLoader.from_pascal_voc(
-        BASE_DIR + '/test_data',
-        BASE_DIR + '/test_data',
-        ['car', 'motorcycle', 'person', 'bus', 'bicycle']
-    )
-
-    models = ["kb4yg_v1.tflite"]
     for m in models:
-        print("\t" + m)
-        # model.evaluate_tflite('kb4yg_v1.tflite', test_data)
+        evaluate_tflite(BASE_DIR + "/models/" + m + "/detect.tflite", "/home/erik/PycharmProjects/ml/test_data", 0.50)
 
-        spec = model_spec.get('efficientdet_lite0')
-        model = object_detector.create(test_data, model_spec=spec, do_train=False)
-
-        # only seems to work on models that have the labelmap inside the tflite metadata, so can't run on models with
-        # labelmap
-        result = model.evaluate_tflite("/home/erik/PycharmProjects/ml/models/" + m, test_data)
-        print(result)
 
 
 if __name__ == "__main__":
--- benchmark/tf_evaluation.py
+++ benchmark/tf_evaluation.py
@@ -10,6 +10,9 @@ import os
 import tensorflow as tf
 import numpy as np
 import cv2
+import xml.etree.ElementTree as ET
+import xmltodict
+import glob
 
 
 def evaluate_tflite_google(model):
@@ -33,7 +36,61 @@ def evaluate_tflite_google(model):
     output_data = interpreter.get_tensor(output_details[0]['index'])
     print(output_data)
 
-def evaluate_tflite(model, img_dir):
+
+def getObjects(xmlFile):
+    xmlFile = xmlFile[:-3] + "xml"
+
+    with open(xmlFile) as file:
+        data = file.read()
+        xml = xmltodict.parse(data)
+        boxes = []
+        objects = xml['annotation']["object"]
+        if not isinstance(objects, list):
+            objects = [objects]
+
+        for obj in objects:
+            # name = obj['name']
+            box = obj['bndbox']
+            xmin = int(box['xmin'])
+            ymin = int(box['ymin'])
+            xmax = int(box['xmax'])
+            ymax = int(box['ymax'])
+
+            tr = (xmax, ymax)  # Top right
+            bl = (xmin, ymin)  # Bottom left
+
+            boxes.append([bl, tr])
+
+    return boxes
+
+
+# returns true if two boxes overlap
+def doBoxesOverlap(box_1, box_2):
+    if (box_1[0] >= box_2[2]) or (box_1[2] <= box_2[0]) or (box_1[3] <= box_2[1]) or (box_1[1] >= box_2[3]):
+        return False
+    else:
+        return True
+
+
+def inArea(box_1, box_2, percentage):
+    # Compute the area of the intersection, which is a rectangle too:
+    dx = max(0, min(box_1[1][0], box_2[1][0]) - max(box_1[0][0], box_2[0][0]))
+    dy = max(0, min(box_1[1][1], box_2[1][1]) - max(box_1[0][1], box_1[0][1]))
+
+    intersecting_area = dx * dy
+    b1_area = (box_1[1][0] - box_1[0][0]) * (box_1[1][1] - box_1[0][1])
+    b2_area = (box_2[1][0] - box_2[0][0]) * (box_2[1][1] - box_2[0][1])
+
+    overlap = intersecting_area / (b1_area + b2_area - intersecting_area)
+
+    if overlap >= percentage:
+        print("\t %5.2f%", overlap)
+        return True
+    else:
+        return False
+
+
+def evaluate_tflite(model, img_dir, min_conf_threshold):
     # Load the TFLite model and allocate tensors.
     interpreter = tf.lite.Interpreter(model_path=model)
     interpreter.allocate_tensors()
@@ -51,11 +108,11 @@ def evaluate_tflite(model, img_dir):
     input_std = 127.5
 
     # test_data the model on each image
-    pr_sum = re_sum = F1_sum = 0
-    images_list = os.listdir(img_dir)
-    for file in images_list:
+    images = glob.glob(img_dir + "/*.jpg")
+    for file in images:
+        print(file)
         # Load image and resize to expected shape [1xHxWx3]
-        image = cv2.imread(img_dir +"/"+file)
+        image = cv2.imread(file)
         if image is None:
             print("Image not found, check path")
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -72,33 +129,34 @@ def evaluate_tflite(model, img_dir):
 
         # Retrieve detection results
         boxes = interpreter.get_tensor(output_details[0]['index'])[0]  # Bounding box coordinates of detected objects
-        classes = interpreter.get_tensor(output_details[1]['index'])[0]  # Class index of detected objects
         scores = interpreter.get_tensor(output_details[2]['index'])[0]  # Confidence of detected objects
 
-        print("Compare with expected")
+        expected = getObjects(file)
 
+        # Loop over all detections and draw detection box if confidence is above minimum threshold
+        results = []
+        for i in range(len(scores)):
+            if (scores[i] > min_conf_threshold) and (scores[i] <= 1.0):
+                # Get bounding box coordinates and draw box Interpreter can
+                # return coordinates that are outside of image dimensions,
+                # need to force them to be within image using max() and min()
+                ymin = int(max(1, (boxes[i][0] * imH)))
+                xmin = int(max(1, (boxes[i][1] * imW)))
+                ymax = int(min(imH, (boxes[i][2] * imH)))
+                xmax = int(min(imW, (boxes[i][3] * imW)))
 
+                # Corners of the bounding box
+                tr = (xmax, ymax)  # Top right
+                bl = (xmin, ymin)  # Bottom left
+                results.append([bl, tr])
 
+        count = 0
+        for res in results:
+            for box in expected:
+                if inArea(res, box, 0.01): #TODO optmi for label .. only cars
+                    count += 1
+                    break
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        print("found:", count)
+        print("expected:", len(expected))
 
